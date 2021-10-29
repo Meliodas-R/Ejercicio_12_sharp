5ba9b1bee01db95d91b8c01c6bef0fa0c1912d8d
Meliodas-R <inceptiongodmaxwell@gmail.com>
2021-10-29 13:00:34 +0200

    Agregada la función para calcular el área , el perímetro y la muestra por pantalla de los resultados.

--- Ejercicio_12_sharp/Program.cs
+++ Ejercicio_12_sharp/Program.cs
@@ -6,7 +6,20 @@ namespace Ejercicio_12_sharp
     {
         static void Main(string[] args)
         {
-            
+            #region Declaracion de variables
+            const double PI = Math.PI;
+            double area;
+            double circunferencia;
+            #endregion
+
+            Console.Write("Introduzca el radio del circulo: ");
+            double radio = Convert.ToDouble(Console.ReadLine());
+
+            area = PI * (Math.Pow(radio, 2));
+            circunferencia = 2 * PI * radio;
+
+            Console.WriteLine("Área del círculo: " + Math.Round(area, 2));
+            Console.WriteLine("Perímetro del circulo: " + Math.Round(circunferencia, 2));
         }
     }
 }
